92f2a01a223cc4ac2db67c015b89aa3e80808c14
BenjaminJHamilton <bjh685@gmail.com>
2025-04-07 19:18:20 +0100

    Fixed test issues

--- tests/test_accounts.py
+++ tests/test_accounts.py
@@ -15,7 +15,7 @@ def test_deposit():
     account = SavingsAccount("isa")
     assert account.balance == 0
     account.deposit(1000)
-    assert account.balance == 0
+    assert account.balance == 1000
 
 def test_lisa_deposit():
 
